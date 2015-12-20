707d0027a465d984a24720e776416e76b73d4665
Brandon Espinoza <espinozab100@gmail.com>
2015-12-19 21:21:48 -0600

    Added stripe implementation with basic design

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,27 @@
+class ChargesController < ApplicationController
+  def create
+    product = Product.find(params[:product_id])
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :card  => params[:stripeToken],
+      :plan  => "GROWHACK2"
+    )
+
+    # charge = Stripe::Charge.create(
+    #   :customer    => customer.id,
+    #   :amount      => product.price_in_cents,
+    #   :description => product.full_description,
+    #   :currency    => 'usd'
+    # )
+
+    purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: product.price_in_cents, description: product.full_description, currency: 'usd', customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+
+    redirect_to purchase
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+
+end
